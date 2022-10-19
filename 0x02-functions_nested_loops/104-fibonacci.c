7950678b4916016d691ec84fca611fe6646bd95c
AmbaliOladele <oladelebidemiweb@gmail.com>
2022-10-19 22:37:44 +0000

    fixed last fibonnaci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,43 +8,43 @@
 
 int main(void)
 {
-	int c, boolean, boolean2;
-	long int n1, n2, fn, fn2, n11, n22;
+	int number, checkNumber, checkNumber2, modF2;
+	long int num1, num2, fn, fn2, num11, num22;
 
-	n1 = 1;
-	n2 = 2;
-	boolean =  boolean2 = 1;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 96; c++)
+	num1 = 1, num2 = 2;
+	checkNumber = checkNumber2 = 1;
+	printf("%ld, %ld", num1, num2);
+	for (number = 0; number < 96; number++)
 	{
-		if (boolean)
+		if (checkNumber)
 		{
-			fn = n1 + n2;
+			fn = num1 + num2;
 			printf(", %ld", fn);
-			n1 = n2;
-			n2 = fn;
+			num1 = num2;
+			num2 = fn;
 		}
 		else
 		{
-			if (boolean2)
+			if (checkNumber2)
 			{
-				n11 = n1 % 1000000000;
-				n22 = n2 % 1000000000;
-				n1 = n1 / 1000000000;
-				n2 = n2 / 1000000000;
-				boolean2 = 0;
+				num11 = num1 % 1000000000;
+				num1 /= 1000000000;
+				num22 = num2 % 1000000000;
+				num2 /= 1000000000;
+				checkNumber2 = 0;
 			}
-			fn2 = (n11 + n22);
-			fn = n1 + n2 + (fn2 / 1000000000);
+			modF2 = fn2 % 1000000000;
+			fn2 = num11 + num22;
+			fn = (num1 + num2) + (modF2);
 			printf(", %ld", fn);
-			printf("%ld", fn2 % 1000000000);
-			n1 = n2;
-			n11 = n22;
-			n2 = fn;
-			n22 = (fn2 % 1000000000);
+			printf("%d", modF2);
+			num1 = num2;
+			num11 = num22;
+			num2 = fn;
+			num22 = modF2;
 		}
-		if (((n1 + n2) < 0) && boolean == 1)
-			boolean = 0;
+		if (((num1 + num2) < 0) && checkNumber == 1)
+			checkNumber = 0;
 	}
 	printf("\n");
 	return (0);
