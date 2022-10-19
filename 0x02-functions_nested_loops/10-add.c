9eab8d2d56cd0d1164f19dd078fb2d86acc09cf1
AmbaliOladele <oladelebidemiweb@gmail.com>
2022-10-19 02:53:36 +0000

    adds two intergers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ *
+ * @num1: The first interger to be added
+ * @num2: The second interger
+ *
+ * Return: num1 + num2;
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
