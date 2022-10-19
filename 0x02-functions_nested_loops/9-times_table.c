a3a6e8613a2c73c9e2b8c12a0fb6c0086ea16598
AmbaliOladele <oladelebidemiweb@gmail.com>
2022-10-19 02:48:34 +0000

    prints times table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ */
+
+void times_table(void)
+{
+	int num, multi, output;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+
+		for (multi = 1; multi <= 9; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			output = num * multi;
+
+			if (output <= 9)
+				_putchar(' ');
+			else
+				_putchar((output / 10) + 48);
+
+			_putchar((output % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
