8339ef54eeff257f09bac848e41b1f4df9a63275
AmbaliOladele <oladelebidemiweb@gmail.com>
2022-10-20 16:14:14 +0000

    draws a line on the terminal ch

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,14 @@
 
 /**
  * print_line - draws a line on the terminal
- * @n - the number of line to draw
+ * @n: the number of line to draw
  */
 
 void print_line(int n)
 {
 	int line;
 
-	for (line = 0; line <= n; line++)
+	for (line = 0; line < n; line++)
 		_putchar(95);
 	_putchar('\n');
 }
